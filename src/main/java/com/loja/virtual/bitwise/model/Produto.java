861c806446849bf8e25318990938f46d3390b772
FireXtz <phantomrising@protonmail.com>
2023-11-19 15:51:08 -0300

    novas classes de modelos referente a desconto, produto e nota

---
+++ src/main/java/com/loja/virtual/bitwise/model/Produto.java
@@ -0,0 +1,192 @@
+package com.loja.virtual.bitwise.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "produto")
+@SequenceGenerator(name = "seq_produto", sequenceName = "seq_produto",allocationSize = 1, initialValue = 1)
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_produto")
+    private Long id;
+
+    private String tipoUnidade;
+
+    private String nome;
+
+    @Column(columnDefinition = "text" , length = 2000)
+    private String descricao;
+
+    private Double peso;
+
+    private Double largura;
+
+    private Double altura;
+
+    private Double profundidade;
+    @ManyToOne
+    @JoinColumn(name = "nota_fiscal_compra_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_compra_fk"))
+    private NotaFiscalCompra notaFiscalCompra;
+
+    private BigDecimal valorVenda = BigDecimal.ZERO;
+
+    private Integer qtdEstoque = 0;
+
+    private Integer qtdAlertaEstoque = 0;
+
+    private String linkYoutube;
+
+    private Boolean alertarQtdEtoque = Boolean.FALSE;
+
+    private Boolean ativo = Boolean.TRUE;
+
+    private Integer qtdClique = 0;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoUnidade() {
+        return tipoUnidade;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public void setNotaFiscalCompra(NotaFiscalCompra notaFiscalCompra) {
+        this.notaFiscalCompra = notaFiscalCompra;
+    }
+
+    public NotaFiscalCompra getNotaFiscalCompra() {
+        return notaFiscalCompra;
+    }
+
+    public void setTipoUnidade(String tipoUnidade) {
+        this.tipoUnidade = tipoUnidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public Double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getProfundidade() {
+        return profundidade;
+    }
+
+    public void setProfundidade(Double profundidade) {
+        this.profundidade = profundidade;
+    }
+
+    public BigDecimal getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(BigDecimal valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public Integer getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(Integer qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public Integer getQtdAlertaEstoque() {
+        return qtdAlertaEstoque;
+    }
+
+    public void setQtdAlertaEstoque(Integer qtdAlertaEstoque) {
+        this.qtdAlertaEstoque = qtdAlertaEstoque;
+    }
+
+    public String getLinkYoutube() {
+        return linkYoutube;
+    }
+
+    public void setLinkYoutube(String linkYoutube) {
+        this.linkYoutube = linkYoutube;
+    }
+
+    public Boolean getAlertarQtdEtoque() {
+        return alertarQtdEtoque;
+    }
+
+    public void setAlertarQtdEtoque(Boolean alertarQtdEtoque) {
+        this.alertarQtdEtoque = alertarQtdEtoque;
+    }
+
+    public Integer getQtdClique() {
+        return qtdClique;
+    }
+
+    public void setQtdClique(Integer qtdClique) {
+        this.qtdClique = qtdClique;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
